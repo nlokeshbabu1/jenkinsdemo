a04c3eea302d022f4e8609474393846d131dc5d8
nlokeshbabu <lokesh.n@aksharags.com>
2023-05-06 16:19:45 +0530

    add Hello.java

---
+++ Hello.java
@@ -0,0 +1,18 @@
+public class Hello {
+	public static void main(String[] args) {
+//		String firstname = "nalluri";
+//		String lastname = "lokesh";
+//		String fullname = firstname + lastname;
+//		System.out.println(fullname);
+		int x=100;
+		int y=200;
+		int z=300;
+		System.out.println(x+y+z);
+		int w=5;
+	        System.out.println(w > 4 && w < 3);
+		if (20 > 18) {
+			System.out.println("20 greater than 18");
+		}
+                		
+  }
+}  
