5453232eb791a49f5e947d44c0e5786ca06d531b
nlokeshbabu1 <138300460+nlokeshbabu1@users.noreply.github.com>
2024-04-15 12:02:12 +0530

    Update start_container.sh

--- script/start_container.sh
+++ script/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull nlokeshbabu/simple-python-app
+docker pull nlokeshbabu/simple-python-flask-app2
 
 # Run the Docker image as a container
-docker run -dit --name sample-python -p 80:80 nlokeshbabu/simple-python-app
+docker run -dit --name sample-python -p 80:80 nlokeshbabu/simple-python-flask-app2
